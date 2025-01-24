7d9a37fd9a92f4afd62906825311768036e45037
OmriTafla <omritafla@gmail.com>
2025-01-24 12:32:34 +0200

    bubbles spawn with size and graphics

--- Assets/Scripts/BubbleGraphics.cs
+++ Assets/Scripts/BubbleGraphics.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class BubbleGraphics : MonoBehaviour
 {
     public Recipe recipe;
-
+    [SerializeField] private SpriteRenderer color, sparkles;
+    [SerializeField] private float[] sizes;
+    [SerializeField] private Sprite[] colorSprites;
+    [SerializeField] private Sprite[] sparkleSprites;
+    [SerializeField] private Sprite defaultColor, defaultSparkles;
     public void SetRecipe(Recipe recipe)
     {
         this.recipe = new Recipe(recipe);
+        color.sprite = recipe.color == -1 ? defaultColor : colorSprites[recipe.color];
+        sparkles.sprite = recipe.sparkles == -1 ? defaultSparkles : sparkleSprites[recipe.sparkles];
+        transform.localScale = new Vector3(1f, 1f, 1f) * sizes[recipe.size];
     }
 }
